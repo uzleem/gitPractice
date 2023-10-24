2862ee704d8cc9d13ca8fbf62ba20542f026ace6
uzleem <uzleem@github.com>
2023-10-24 13:05:01 +0900

    stash

--- calculator.js
+++ calculator.js
@@ -1,5 +1,4 @@
 # 계산기
-// update : 20:20
 function add(a,b) {
 	return a+b;
 
@@ -9,9 +8,9 @@ function subtract(a,b) {
 	return a-b;
 }
 
-
 function main(a,b) {
 	return a/b;
+}
 
 function merrge(a,b) {
 	return a*b;
